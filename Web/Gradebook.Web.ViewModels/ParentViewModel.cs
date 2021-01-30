7b1eaf6c08215d85e390964c505f29e34f5fa40b
Plotso <ilian_1999n@abv.bg>
2021-01-31 00:41:29 +0200

    Added page to show Class details, where you can see information about each student and his/her parent(s)

--- Web/Gradebook.Web.ViewModels/ParentViewModel.cs
+++ Web/Gradebook.Web.ViewModels/ParentViewModel.cs
@@ -7,6 +7,8 @@
 
     public class ParentViewModel : IMapFrom<Parent>
     {
+        public int Id { get; set; }
+        
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
